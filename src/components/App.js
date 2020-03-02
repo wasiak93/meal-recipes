3faf442cffd10909500b8f59631aa8e55412ae22
kacper <wasiak93@github.com>
2020-03-02 18:10:38 +0100

    show info no results

--- src/components/App.js
+++ src/components/App.js
@@ -3,47 +3,54 @@ import "./App.scss";
 import Form from "./Form/Form";
 import Result from "./ResultsList/List";
 import { trackPromise } from "react-promise-tracker";
+import { useEffect } from "react";
 
 function App() {
   const name = "chicken";
-  const [data, setData] = useState([]);
+  const [data, setData] = useState("");
   const [value, setValue] = useState("");
   const [isSearch, setSearch] = useState(false);
+  const [showNoResults, setShow] = useState(false);
 
   const handleForm = e => {
     e.preventDefault();
-    setSearch(true);
-    trackPromise(
-      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
-        .then(response => {
-          if (response.ok) {
+    if (value.length >= 2) {
+      trackPromise(
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
+          .then(response => {
+            if (response.ok) {
+              return response;
+            } else {
+              throw Error("Something gone wrong :(");
+            }
+          })
+          .then(response => {
             return response.json();
-          } else {
-            throw Error("Something gone wrong :(");
-          }
-        })
-
-        .then(resp => setData([...resp.meals]), [])
-        .catch(error => console.log(error))
-    );
+          })
+          .then(resp => {
+            if (resp.meals) {
+              setData([...resp.meals]);
+            } else setData("");
+          }, [])
+          .catch(error => console.log(error))
+      );
+    } else setData("");
   };
 
+  // useEffect(() => {
+  //   console.log(typeof data);
+  //   console.log(showNoResults);
+  //   if (typeof data == "string") return setShow(true);
+  // }, [data, showNoResults]);
   const handleInput = e => {
     setValue(e.target.value);
   };
-
-  let result = "";
-  if (data.length >= 1) {
-    result = <Result data={data} />;
-  } else if (isSearch && typeof data.length >= 1) {
-    result = (
-      <p className="app__no-result-info"></p>
-    )`Upsss, ther isn't any recipes for '${value} `;
-  }
   return (
     <div className="app">
       <Form submit={handleForm} value={value} handleInput={handleInput} />
-      {result}
+      {/* {showNoResults ? <p>ahoj</p> : null} */}
+      {data === "" ? <p>no result</p> : null}
+      {data.length >= 1 ? <Result data={data} /> : null}
     </div>
   );
 }
