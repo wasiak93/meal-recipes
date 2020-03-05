3a69525f0e228fc853d4fba78b7730ffb81a91ec
kacper <wasiak93@github.com>
2020-03-05 14:04:32 +0100

    remove fetch from App.js, add fetch in Result.js- useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -2,82 +2,34 @@ import React, { useState } from "react";
 import "./App.scss";
 import Form from "./Form/Form";
 import Results from "./ResultsList/Results";
-import { trackPromise } from "react-promise-tracker";
 
 function App() {
-  const [data, setData] = useState([]);
   const [inputValue, setValue] = useState("");
   const [searchValue, setSearch] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleForm = e => {
     e.preventDefault();
-    if (inputValue.length >= 2) {
-      setSearch(inputValue);
-      setIsLoading(true);
-      trackPromise(
-        fetch(
-          `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
-        )
-          .then(response => {
-            if (response.ok) {
-              return response;
-            } else {
-              throw Error("Something gone wrong :(");
-            }
-          })
-          .then(response => {
-            return response.json();
-          })
-          .then(
-            resp => {
-              if (resp.meals) {
-                setData([...resp.meals]);
-              } else setData(false);
-              setIsLoading(false);
-            },
-
-            []
-          )
-          .catch(error => {
-            console.log(error);
-            setIsLoading(false);
-          })
-      );
-    } else {
-      setData(true);
-    }
+    setSearch(inputValue);
   };
 
   const handleInput = e => {
     setValue(e.target.value);
   };
 
-  let results = "";
+  let result = "";
 
-  if (!isLoading && !data) {
-    results = (
-      <p className="wrapper__results-info"> no result for "{searchValue}"</p>
-    );
-  } else if (inputValue.length <= 1 && data === true) {
-    results = (
+  if (searchValue !== false)
+    result = (
       <p className="wrapper__results-info">please type at least two chars</p>
     );
-  } else if (data.length >= 1 && searchValue) {
-    results = (
-      <>
-        <p className="wrapper__results-info">
-          results for "{searchValue}"<span>({data.length}):</span>
-        </p>
-        <Results data={data} search={searchValue} />
-      </>
-    );
-  }
 
+  if (searchValue.length >= 2) {
+    result = <Results search={searchValue} />;
+  }
   return (
     <div className="wrapper">
       <Form submit={handleForm} value={inputValue} handleInput={handleInput} />
-      {results}
+      {result}
     </div>
   );
 }
--- src/components/ResultsList/Results.js
+++ src/components/ResultsList/Results.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import List from "./List";
 import "./Results.scss";
+import { trackPromise } from "react-promise-tracker";
 
-const Results = ({ data, search }) => {
+const Results = ({ search }) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = useCallback(() => {
+    setIsLoading(true);
+    trackPromise(
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        .then(response => {
+          if (response.ok) {
+            return response;
+          } else {
+            throw Error("Something gone wrong :(");
+          }
+        })
+        .then(response => {
+          return response.json();
+        })
+        .then(
+          resp => {
+            if (resp.meals) {
+              setData([...resp.meals]);
+            } else setData(false);
+            setIsLoading(false);
+          },
+
+          []
+        )
+        .catch(error => {
+          console.log(error);
+          setIsLoading(false);
+        })
+    );
+  }, [search]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  let resultsInfo = "";
+
+  if (data && !isLoading) {
+    resultsInfo = (
+      <p className="wrapper__results-info">
+        results for "{search}"<span>({data.length}):</span>
+      </p>
+    );
+  } else if (!data && !isLoading) {
+    resultsInfo = (
+      <p className="wrapper__results-info"> no results for "{search}"</p>
+    );
+  }
+
   return (
     <div className="results app__results">
-      <List data={data} />
+      {resultsInfo}
+      {data && <List data={data} />}
     </div>
   );
 };
