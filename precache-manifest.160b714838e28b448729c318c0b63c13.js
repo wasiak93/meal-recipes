5394963eea6109c35b6d032989b59211b2bb9099
wasiak93 <kacperjacekwasiak@gmail.com>
2020-03-06 17:37:00 +0100

    Updates

---
+++ precache-manifest.160b714838e28b448729c318c0b63c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d32069c3131bffebc5afe452a2e7c57",
+    "url": "/meal-recipes/index.html"
+  },
+  {
+    "revision": "80e08f14455b1ccd97a9",
+    "url": "/meal-recipes/static/css/main.9c23f1d7.chunk.css"
+  },
+  {
+    "revision": "863e8d07be8128e6b29d",
+    "url": "/meal-recipes/static/js/2.544cd186.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/meal-recipes/static/js/2.544cd186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e08f14455b1ccd97a9",
+    "url": "/meal-recipes/static/js/main.da0f7c7d.chunk.js"
+  },
+  {
+    "revision": "623ca09087acc0ab9d5c",
+    "url": "/meal-recipes/static/js/runtime-main.a6deb911.js"
+  }
+]);
